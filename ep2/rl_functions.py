71bd3b5721488f3b4dbabb95714330b55a8d60d5
Diego Mansilla <dmansilla07@gmail.com>
2018-06-28 19:17:02 -0300

    Kaggle

--- ep2/rl_functions.py
+++ ep2/rl_functions.py
@@ -26,11 +26,9 @@ def standardize(X):
     :return: standardized array
     :rtype: np.ndarray(shape=(N, d))
     """
-
-    # YOUR CODE HERE:
-    raise NotImplementedError
-    # END YOUR CODE
-
+    X_out = X.copy()
+    X_out -= np.mean(X)
+    X_out /= np.std(X)
     return X_out
 
 
@@ -43,16 +41,13 @@ def compute_cost(X, y, w):
     :param y: regression targets
     :type y: np.ndarray(shape=(N, 1))
     :param w: weights
-    :type w: np.array(shape=(d,))
+    :type w: np.array(shape=(d, 1))
     :return: cost
     :rtype: float
     """
-
-    # YOUR CODE HERE:
-    raise NotImplementedError
-    # END YOUR CODE
-
-    return J
+    N = X.shape[0]
+    J = 1.0/N*np.dot(np.transpose(np.matmul(X, w) - y), np.matmul(X, w) - y)
+    return J[0][0]
 
 
 def compute_wgrad(X, y, w):
@@ -64,15 +59,17 @@ def compute_wgrad(X, y, w):
     :param y: regression targets
     :type y: np.ndarray(shape=(N, 1))
     :param w: weights
-    :type w: np.array(shape=(d,))
+    :type w: np.array(shape=(d, 1))
     :return: gradient
-    :rtype: np.array(shape=(d,))
+    :rtype: np.array(shape=(d, 1))
     """
-
-    # YOUR CODE HERE:
-    raise NotImplementedError
-    # END YOUR CODE
-
+    yy = np.matmul(X, w)
+    N = X.shape[0]
+    M = X.shape[1]
+    grad = np.ndarray(shape=(M, 1))
+    for j in range(0, M):
+        sum2 = (np.dot(np.transpose(X)[j], (yy-y)))*2/N
+        grad[j] = sum2
     return grad
 
 
@@ -85,23 +82,24 @@ def batch_gradient_descent(X, y, w, learning_rate, num_iters):
     :param y: regression targets
     :type y: np.ndarray(shape=(N, 1))
     :param w: weights
-    :type w: np.array(shape=(d,))
+    :type w: np.array(shape=(d, 1))
     :param learning_rate: learning rate
     :type learning_rate: float
     :param num_iters: number of iterations
     :type num_iters: int
     :return: weights, weights history, cost history
-    :rtype: np.array(shape=(d,)), list, list
+    :rtype: np.array(shape=(d, 1)), list, list
     """
-
-    weights_history = [w]
+    weights_history = [w.flatten()]
+    
     cost_history = [compute_cost(X, y, w)]
+    for i in range(0, num_iters):
+        grad = compute_wgrad(X, y, w)
+        w = w - learning_rate*grad
+        weights_history.append(w.flatten())
+        cost_history.append(compute_cost(X, y, w))
 
-    # YOUR CODE HERE:
-    raise NotImplementedError
-    # END YOUR CODE
-
-    return w, weights_history, cost_history
+    return w, weights_history, cost_history 
 
 
 def stochastic_gradient_descent(X, y, w, learning_rate, num_iters, batch_size):
@@ -123,8 +121,22 @@ def stochastic_gradient_descent(X, y, w, learning_rate, num_iters, batch_size):
     :return: weights, weights history, cost history
     :rtype: np.array(shape=(d, 1)), list, list
     """
-    # YOUR CODE HERE:
-    raise NotImplementedError
-    # END YOUR CODE
-
-    return w, weights_history, cost_history
+    new_w = w.copy()
+    N = X.shape[0]
+    m = batch_size
+    weights_history = [w.flatten()]
+    cost_history = [compute_cost(X, y, w)]
+    
+    for i in range(0, num_iters):
+        p = np.random.permutation(N)
+        np.random.shuffle(p)
+        new_x = np.ndarray(shape=(m, X.shape[1]))
+        new_y = np.ndarray(shape=(m, y.shape[1]))
+        for j in range(0, m):
+            new_x[j] = X[p[j]]
+            new_y[j] = y[p[j]]
+        grad = compute_wgrad(new_x, new_y, new_w)
+        new_w = new_w - learning_rate*grad
+        weights_history.append(new_w.flatten())
+        cost_history.append(compute_cost(X, y, new_w))    
+    return np.asarray(new_w), weights_history, cost_history
